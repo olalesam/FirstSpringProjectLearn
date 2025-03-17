b23522c4d5aced34b47d4ccfadfd7a4be20c08e0
Coumba <olalesam87@gmail.com>
2025-03-17 16:18:40 +0000

    endpoint get product by id

---
+++ src/main/java/com/nadhem/produits/restcontrollers/ProduitRestController.java
@@ -0,0 +1,33 @@
+package com.nadhem.produits.restcontrollers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nadhem.produits.entities.Produit;
+import com.nadhem.produits.services.ProduitService;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ProduitRestController {
+	
+	@Autowired
+	ProduitService produitService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Produit> getAllProduit() {
+		return produitService.getAllProduits();
+	}
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+	public Produit getProduitById(@PathVariable("id") Long id) {
+		return produitService.getProduit(id);
+	 }
+	
+}
