77250daeb9c99d87497b6d14b12415043f48494b
Coumba <olalesam87@gmail.com>
2025-03-17 16:44:33 +0000

    endpoint create product

--- src/main/java/com/nadhem/produits/restcontrollers/ProduitRestController.java
+++ src/main/java/com/nadhem/produits/restcontrollers/ProduitRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +30,10 @@ public class ProduitRestController {
 	public Produit getProduitById(@PathVariable("id") Long id) {
 		return produitService.getProduit(id);
 	 }
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public Produit createProduit(@RequestBody Produit produit) {
+		return produitService.saveProduit(produit);
+	}
 	
 }
