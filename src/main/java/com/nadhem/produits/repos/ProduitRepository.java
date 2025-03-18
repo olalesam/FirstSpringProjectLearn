029fb8b5d34b8a6f2d18236f8e7c1a7fa860b861
Coumba <olalesam87@gmail.com>
2025-03-18 15:24:55 +0000

    using Spring Data REST

--- src/main/java/com/nadhem/produits/repos/ProduitRepository.java
+++ src/main/java/com/nadhem/produits/repos/ProduitRepository.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.nadhem.produits.entities.Categorie;
 import com.nadhem.produits.entities.Produit;
 
 
-
+@RepositoryRestResource(path = "rest") 
 public interface ProduitRepository extends JpaRepository<Produit, Long > {
 	
 	List<Produit> findByNomProduit(String nomPrduit);
